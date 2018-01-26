c91322d99e854378adac40815d9d3214c611158f
Liad Idan <liad.idan@gmail.com>
2018-01-26 21:20:52 +0200

    Update service worker config

--- angular-src/src/app/core/services/connection.service.ts
+++ angular-src/src/app/core/services/connection.service.ts
@@ -1,7 +1,8 @@
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConnectionService {
-  public serverUrl = 'http://127.0.0.1:3000';
+  public serverUrl = environment.production ? 'https://vegannearby.com' : 'http://127.0.0.1:3000';
   public reqTimeout = 5000;
 }
